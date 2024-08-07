5e010b75f32924690287363a0aaabeb1916cc134
Harsh Kedia <hkedia@hey.com>
2024-08-07 18:55:00 +0530

    the duper app

--- lib/duper/application.ex
+++ lib/duper/application.ex
@@ -8,12 +8,15 @@ defmodule Duper.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      Duper.Results
+      Duper.Results,
+      {Duper.PathFinder, "/home/hkedia/MEGA/Photos"},
+      Duper.WorkerSupervisor,
+      {Duper.Gatherer, 2}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Duper.Supervisor]
+    opts = [strategy: :one_for_all, name: Duper.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
